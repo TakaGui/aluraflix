b31870b24ea9447bb54c748bf4b2e4b355ba9559
TakaGui <gui.yukio175@gmail.com>
2020-07-28 13:08:03 -0300

    slider carousel

--- src/pages/Upload/Category/index.js
+++ src/pages/Upload/Category/index.js
@@ -7,7 +7,20 @@ function UploadCategory() {
   return (
     <PageDefault>
       <h1>Cadastro de categoria</h1>
+      
+      <form>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
 
+        <button>
+          Cadastrar
+        </button>
+      </form>
+      
       <Link to="/">
         Ir para home
       </Link>
